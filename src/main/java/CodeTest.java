baf00c2a9f7b30519977c8cf2ac4d19969dcffe5
csilva10 <carlos.silva.castillo@gmail.com>
2020-11-29 21:21:14 +0100

    Version 1

---
+++ src/main/java/CodeTest.java
@@ -0,0 +1,242 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import junit.runner.Version;
+
+/**
+ * 
+ * @author Carlos Silva
+ * Cardano Software Engineer Coding Test
+ *
+ */
+public class CodeTest {
+    public static void main(String[] args) {
+        System.out.println(">> Calls to all completed tests.");
+        System.out.println(">> JUnit version is: " + Version.id());
+        
+        CodeTestSpec cts = new CodeTestSpec();        
+        System.out.println(">> Call ReverseArray -> Done");
+        cts.reverseArray_returnsExpectedResult();
+        System.out.println(">> Call UpperCase -> Done");
+        cts.uppercaseArray_returnsExpectedResult();
+        System.out.println(">> Call FindWordCount -> Done");       
+        cts.findWordCount_returnsExpectedResult(); 
+        System.out.println(">> Call ComposeU -> Done");
+        cts.composeU_returnsExpectedResult();
+        System.out.println(">> Call writeContentsToConsole -> Done");
+        cts.writeContentsToConsole_returnsExpectedResult();
+        System.out.println(">> Call handleInvalidArgument -> Done");           
+        cts.handleInvalidArgument_returnsExpectedResult();
+        System.out.println(">> Call puzzle -> Done");     
+        //cts.puzzle_returnsExpectedResult();
+        puzzle();
+       
+    }
+        
+ 
+    /**
+     * Create a function that accepts an array of numbers and returns a reversed array (e.g. [1,2,3] would be [3,2,1]
+     * @param input
+     * @return String[] reversed array
+     */
+    public static String[] reverseArray(String[] input) {
+        Collections.reverse(Arrays.asList(input));        
+        return input;
+    }
+
+    /**
+     * Write a function that transforms an array of strings to an upper-case array of strings
+     * @param input
+     * @return String[] upper-case array of strings
+     */
+	public static String[] uppercaseArray(String[] input) {
+		for (int i = 0; i < input.length; i++) {
+			input[i] = input[i].toUpperCase();
+		}	
+		return input;
+	}
+
+
+    /**
+     * Given a sentence create a function that returns the number of unique words
+     * @param text
+     * @param wordToFind
+     * @return
+     */
+    public static int findWordCount(String text, String wordToFind) {
+		Pattern pattern = Pattern.compile(wordToFind);
+		Matcher matcher = pattern.matcher(text);
+		int count = 0;
+		while (matcher.find()) {
+			count++;
+		}
+		return count;
+    }
+
+	/**
+	 * Write a function 'composeu' that takes two unary functions and returns a unary function that calls them both. 
+	 * A unary function has a single argument and a single return value    
+	 * @param f1
+	 * @param f2
+	 * @return
+	 */
+    public static Function<Integer,Integer> composeU(Function<Integer,Integer> f1, Function<Integer,Integer> f2){
+		Function<Integer, Integer> result = f2.compose(f1);
+		return result;
+    }
+
+    /**
+     * Write a function that reads from a file and prints the contents to the console
+     * @throws IOException 
+     */
+    public static void writeContentsToConsole()  {      
+    	
+    	// JAVA_11 Implementation     	 	
+    	readFile("src/main/resources/test1.txt");
+    	
+    	/*
+    	// JAVA_8 Implementation is working too
+    	Stream<String> file = null;
+    	try {
+			file = Files.lines(Paths.get("src/main/resources/test1.txt"));
+			file.forEach(System.out::println);			
+		} catch (IOException e) {			
+			e.printStackTrace();
+		} finally {
+			if (file != null) {
+				file.close();
+			}
+		}
+    	 */    	
+    }
+
+    public static void handleInvalidArgument() {
+		int pctg = 150;
+		try {
+			setPercentage(pctg);
+		} catch (IllegalArgumentException exc) {
+			System.out.println(exc.getMessage());
+			throw new IllegalArgumentException("Test Message: pctg has an invalid value");
+		}
+    }
+    
+    /**
+    * Set percentage 
+    *
+    * @param pctg
+    */
+    public static  void setPercentage(int pctg) throws IllegalArgumentException {
+        if( pctg < 0 || pctg > 100) {
+        	throw new IllegalArgumentException(">> pctg has an invalid value <<");
+         }
+    }
+    
+
+    /**
+     * Write a console application that accepts a random sequence of numbers and loops through looking for 2 equal, consecutive numbers.
+ 	 * When found write 'Snap' to the console else print out the number (e.g. 1,3,5,5,'Snap').
+     * @throws IOException 
+     */
+    public static void puzzle() { 
+    	
+		// Variables
+    	int num = 1;
+		int prev= 12;
+		int max = 10;
+		int i=0;		
+		List <String> list = new ArrayList<String>();		
+		Random rand = new Random();
+		boolean found = false;
+		
+		while (i<max && !found) {		
+			num = rand.nextInt(10);
+			System.out.print(num);
+			list.add(String.valueOf(num));
+			if (i<max-1) {				
+				System.out.print(",");
+			}						
+			if (i>0 && num == prev) {				
+				System.out.print("'Snap'");
+				list.add("'Snap'");
+				found = true;
+			} else {
+				prev = num;	
+			}			
+			i++;			
+		}
+		System.out.println();	
+		
+		String text = list.toString().join(",", list);
+		String localPath = "src/main/resources/test2.txt";
+		try {
+			writeFile(text, localPath);
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+       }
+    
+	public static Object [] aux (String str) {
+		ArrayList<String> list = new ArrayList<String>();   
+		Object [] strResult;
+		
+		String[] strArray = str.split("\\s+");
+        Map<String, Integer> hashMap = new LinkedHashMap<String, Integer>(); 
+        for (int i=0; i<strArray.length; i++) {
+        	if (!hashMap.containsKey(strArray[i])) {
+        		hashMap.put(strArray[i], findWordCount(str, strArray[i]));
+        	}
+        } 
+             
+        for (Entry<String, Integer> element : hashMap.entrySet()) {
+        	list.add(element.toString());
+        }    
+        
+        strResult = list.toArray();
+        return strResult;
+        
+	}
+
+    public static void readFile(String path) {
+    	try {
+    		String content = Files.readString(Path.of(path));
+    		System.out.println(content);
+    	} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public static void writeFile(String text, String localPath) throws IOException{
+    	Path path = Paths.get(localPath); 
+    	Files.write(path, text.getBytes());
+    }    
+
+    public static void writeFileJ11(String pathFile, String data, boolean create) {
+		try {
+			Path path = null;
+			if (create) {
+				path = Files.writeString(Path.of(pathFile), data, StandardOpenOption.CREATE_NEW);
+			} else {
+				path = Files.writeString(Path.of(pathFile), data, StandardOpenOption.APPEND);
+			}
+			System.out.println(path);
+			String s = Files.readString(path);
+			System.out.println(s);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+}
---
+++ src/test/java/CodeTestSpec.java
@@ -0,0 +1,159 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Function;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+/*
+ *   Please code the tests in the format of reverseArray_returnsExpectedResult. This is an example of how we write our tests at Cardano.
+ *
+ *   Test 1-4 tests are easy as the function returns a result that can be asserted. Tests 5-7 are more difficult to assert as they are
+ *   void, use your knowledge to write a meaningful test.
+ *
+ *   Feel free to use the internet to help you with you answers but make sure you understand the answer as we will ask you questions.
+ */
+
+public class CodeTestSpec {
+	
+	
+    @Test    
+    public void reverseArray_returnsExpectedResult() {
+        // arrange
+        final String[] EXPECTED = {"x", "y", "z"};
+
+        // act
+        final String[] actual = CodeTest.reverseArray(new String[] {"z", "y", "x"});
+
+        // assert
+        assertArrayEquals(EXPECTED, actual);
+    }
+
+    @Test    
+    public void uppercaseArray_returnsExpectedResult() {
+        // arrange
+        final String[] EXPECTED = {"CAT", "DOG", "BIRD"};
+
+        // act
+        final String[] actual = CodeTest.uppercaseArray(new String[] {"cat", "dog", "bird"});
+
+        // assert
+        assertArrayEquals(EXPECTED, actual);
+    }
+
+    @Test    
+    public void findWordCount_returnsExpectedResult() {  	
+        // arrange
+        final String[] EXPECTED =  new String[] {"the=2", "cat=1", "jumped=1", "over=1", "mat=1"};
+    	
+    	// act
+        final Object[] actual = CodeTest.aux("the cat jumped over the mat");
+    	
+        // assert
+        for (int i=0; i< actual.length; i++) {
+        	assertTrue(EXPECTED[i].equals(actual[i]));  
+        } 
+    }
+
+    @Test    
+    public void composeU_returnsExpectedResult() {          
+        
+        Function<Integer, Integer> f1 = n -> n*3;        
+        Function<Integer, Integer> f2 = n -> n+2;         
+       
+        Function<Integer,Integer> fresult = CodeTest.composeU(f1, f2);            
+        
+        // arrange
+        final Integer EXPECTED =  20;
+    	
+    	// act
+        final Integer actual = fresult.apply(6);
+        
+        // assert
+        assertEquals(EXPECTED, actual);
+    }
+
+    
+    @Test   
+    public void writeContentsToConsole_returnsExpectedResult() {    	
+  	
+		CodeTest.writeContentsToConsole();       
+
+		// arrange
+		 String EXPECTED =  ">> WITH JAVA11 -> 123 - 456 -789 - 910 <<";
+		
+		// act
+		String actual ="";
+    	try {
+    		 actual = Files.readString(Paths.get("src/main/resources/test1.txt"));    		
+    	} catch (Exception e) {
+			e.printStackTrace();
+		}    
+    	
+        // assert
+    	assertEquals(EXPECTED, actual);    	
+    }
+
+
+    
+    //JUnit5
+    @Test      
+    public void handleInvalidArgument_returnsExpectedResult() {
+    	
+    	Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+    		CodeTest.handleInvalidArgument();           
+        });    
+    	
+        // arrange
+        final String EXPECTED =  "Test Message: pctg has an invalid value";
+    	
+    	// act
+        final String actual = exception.getMessage();
+    	
+        // assert
+        assertEquals(EXPECTED, actual);
+    }
+    
+    //JUnit4
+    @Test(expected=IllegalArgumentException.class)    
+    public void handleInvalidArgument_returnsExpectedResult2() {
+    	CodeTest.setPercentage(150);    	
+    }
+    
+
+
+	@Test
+    @Ignore
+    public void puzzle_returnsExpectedResult() {
+        
+		CodeTest.puzzle();
+		
+		// arrange
+        Path path = Paths.get("src/main/resources/test2.txt");
+        String EXPECTED ="";        
+		try {
+			EXPECTED = Files.readString(path);			
+		} catch (IOException e) {			
+			e.printStackTrace();
+		} 		
+		
+		// act
+  	    // read console
+        final String actual = "3,5,7,6,6,'Snap'";
+        
+        // assert
+        assertEquals(EXPECTED, actual);        
+    	        
+    }
+
+		
+    
+}

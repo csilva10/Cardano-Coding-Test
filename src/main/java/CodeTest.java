6b86207cec310dfb92d88878a1018d66568d38ae
csilva10 <carlos.silva.castillo@gmail.com>
2020-12-02 12:12:14 +0100

    Version 2.1

--- src/main/java/CodeTest.java
+++ src/main/java/CodeTest.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
